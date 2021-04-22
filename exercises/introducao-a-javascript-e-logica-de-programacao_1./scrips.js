51abb690ccd55e1b036db095b1500129f50a0c23
Martin <escorpmartin97@gmail.com>
2021-04-22 18:38:08 -0300

    aadiciona calculadora

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1./scrips.js
@@ -0,0 +1,22 @@
+let a = 5;
+let b = 10;
+
+//Soma
+console.log('Adicao');
+console.log(a+b);
+
+//Resta
+console.log('Subtração');
+console.log(a-b);
+
+//Multiplicação
+console.log('Multiplicação');
+console.log(a*b);
+
+//Divisão
+console.log('Divisão');
+console.log(a/b);
+
+//Módulo
+console.log('Módulo');
+console.log(a%b);
