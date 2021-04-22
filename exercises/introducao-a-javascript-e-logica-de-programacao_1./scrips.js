3c102b776cc14d6beb71ef541f7ecef768350446
Martin <escorpmartin97@gmail.com>
2021-04-22 19:00:17 -0300

    adiciona maior de tres variaveis

--- exercises/introducao-a-javascript-e-logica-de-programacao_1./scrips.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1./scrips.js
@@ -3,6 +3,11 @@ let b = 10;
 let comp1 = 15;
 let comp2 = 20;
 
+let num1 = 18;
+let num2 = 2;
+let num3 = 25;
+
+//Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
 //Soma
 console.log('Adicao');
 console.log(a+b);
@@ -23,11 +28,23 @@ console.log(a/b);
 console.log('Módulo');
 console.log(a%b);
 
-//Comparação
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
 
-console.log("Numero maior:");
+console.log("Numero maior en duas variaveis:");
 if (comp1>comp2){
     console.log(comp1);
 }else{
 console.log(comp2);
+}
+
+//Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+
+console.log("Numero maior com três variaveis:");
+
+if (num1>num2 && num1>num3){
+    console.log("o numero maior é ",num1);
+}else if (num2>num1 && num2>num3){
+    console.log("o numero maior é ",num2);
+}else{
+    console.log("o numero maior é ",num3)
 }
