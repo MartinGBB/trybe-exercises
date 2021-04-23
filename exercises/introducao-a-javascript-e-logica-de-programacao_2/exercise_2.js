7558354e9eb142f57d9352f8a583464804c30f6e
Martin <escorpmartin97@gmail.com>
2021-04-23 20:15:57 -0300

    adiciona exercise 3

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_2.js
@@ -3,7 +3,6 @@ let soma = 0;
 //2.Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 for(let index1 = 0; index1 < numbers.length; index1 += 1) {
     soma = numbers[index1] + soma;
-    //console.log(numbers[index1]);
 
 };
 console.log(soma);
