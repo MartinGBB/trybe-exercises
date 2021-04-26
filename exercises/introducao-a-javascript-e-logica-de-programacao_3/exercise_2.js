953a23972dc1ea9240dbbe0d3977c869691999a7
Martin <escorpmartin97@gmail.com>
2021-04-26 18:43:18 -0300

    exercise 2

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise_2.js
@@ -0,0 +1,8 @@
+//2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+let n = 5;
+let espaco = "";
+
+for (let index = 0; index < n; index += 1){
+espaco = espaco + '*';
+console.log(espaco);
+}
