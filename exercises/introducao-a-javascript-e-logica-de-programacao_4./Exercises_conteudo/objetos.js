347e290f2e03dc3630bc28c9f4bce1126613d5ed
Martin <escorpmartin97@gmail.com>
2021-04-27 19:18:27 -0300

    adiciona exercise 1, inconcluso

--- exercises/introducao-a-javascript-e-logica-de-programacao_4./Exercises_conteudo/objetos.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4./Exercises_conteudo/objetos.js
@@ -6,11 +6,11 @@ let player = {
     golden: 2,
     silver: 3,
  },
- bestInTheWorl: {
+ bestInTheWorld: {
     bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
  }
 };
 
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' de idade.');
-console.log('A jogadora' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por 6 vezes');
+console.log('A jogadora' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por 6 vezes' + player.bestInTheWorl.bestInTheWorld.length);
 console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro' + ' e ' + player.medals.silver + ' medalhas de prata.');
