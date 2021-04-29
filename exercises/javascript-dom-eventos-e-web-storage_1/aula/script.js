5834f5a90e94b6e9d93e051bf296c2867107fadf
Martin <escorpmartin97@gmail.com>
2021-04-29 20:03:22 -0300

    exercises bloco 5

---
+++ exercises/javascript-dom-eventos-e-web-storage_1/aula/script.js
@@ -0,0 +1,15 @@
+document.getElementById("header-container").style.backgroundColor = "green";
+
+document.getElementsByClassName("color")[1].style.backgroundColor = "red";
+
+document.getElementsByClassName("color")[0].style.backgroundColor = "red";
+
+document.getElementsByClassName("color")[2].style.backgroundColor = "black";
+
+document.getElementsByClassName("color")[3].style.backgroundColor = "black";
+
+document.getElementsByTagName("section")[0].style.backgroundColor = "orange";
+
+document.getElementsByTagName("section")[1].style.backgroundColor = "yellow";
+
+document.getElementsByTagName('div')[4].style.backgroundColor = "black";
