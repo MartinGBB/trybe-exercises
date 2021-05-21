4891e8b0b015515577099cbdfc13287b11e40dad
Martin <escorpmartin97@gmail.com>
2021-05-21 18:53:18 -0300

    exercise2

---
+++ exercises/exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercise_2.js
@@ -0,0 +1,24 @@
+// Crie uma função que receba um número e retorne seu fatorial.
+	const factorial = (numero) => {
+    let num = 1;
+    if (numero > 0) {
+        for (let index = 1; index <= numero; index += 1) {
+            num = num * index;
+        }
+        console.log(num);
+    }
+	}
+	console.log(factorial(4));
+
+//Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
+
+const factorial = (numero) => { (numero > 0 ? (numero * factorial(numero - 1)) : 1)
+	// if (numero == 0) {
+	// 	return 1;
+	// }
+	// else if (numero > 0) {
+	// return numero * factorial(numero - 1);
+	// }
+}
+
+console.log(factorial(4));
