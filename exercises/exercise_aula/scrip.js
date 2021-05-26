8f190195c8b2b3c817269309feae7608fa35251f
Martin <escorpmartin97@gmail.com>
2021-05-26 20:22:37 -0300

    exercise 1 find

--- exercises/exercise_aula/scrip.js
+++ exercises/exercise_aula/scrip.js
@@ -15,4 +15,13 @@ const emailListInData = [
     showEmailList(item)
   });
 
-  
+    //array find
+    
+  // 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
+
+const numbers = [19, 20, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = (number) => (number % 3 === 0 && number % 5 === 0);
+
+const verify = numbers.find(findDivisibleBy3And5)
+console.log(findDivisibleBy3And5());
