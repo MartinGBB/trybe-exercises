48c7d9300576d01b19509a000f5557dbdd9f93c7
Martin <escorpmartin97@gmail.com>
2021-06-26 15:53:31 -0300

    ultimo commit

--- exercises/10.1/exercise_1.test.js
+++ exercises/10.1/exercise_1.test.js
@@ -2,6 +2,15 @@ const sum = require('./exercise_1');
 
 describe('Requisito 1', () => {
     it('A função soma 4 e 5 e retorna 9', () => {
-        expect(9).toBe(sum(4, 5));
-    })
+        expect(sum(4, 5)).toBe(9);
+    });
+
+    it('A função soma 0 e 0 e retorna 0', () => {
+        expect(sum(0, 0)).toBe(0);
+    });
+    it('Erro se o type é string', () => {
+        expect(() => {
+            sum(4, '5')
+        }).toThrow();
+    });
 })
