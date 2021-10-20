701a12cb49140f233754f9e46c7ece3643bed0e6
Martin <escorpmartin97@gmail.com>
2021-10-20 19:24:08 -0300

    [calculo dinamico]

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/imc/imc.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/imc/imc.js
@@ -1,7 +1,6 @@
-
-
 const imc = (peso, altura) => {
   const calculo = Number(peso / Math.pow(altura / 100, 2)).toFixed(2);
-  return console.log(`imc: ${calculo}`);
-}
-imc(peso, altura);
+  return console.log(`Teu imc é: ${calculo}`);
+};
+
+module.exports = imc;
--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/imc/index.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/imc/index.js
@@ -1,3 +1,9 @@
 const readline = require('readline-sync');
 const calculoImc = require('./imc');
 
+const peso = readline.questionFloat(`Qual seu peso? \n`);
+const altura = readline.questionFloat("Qual sua altura? \n");
+
+const imc = calculoImc(peso, altura);
+
+console.log(`Teu imc es: ${imc}`);
