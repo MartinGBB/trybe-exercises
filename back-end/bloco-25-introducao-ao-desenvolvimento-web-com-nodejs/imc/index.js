baba2cd309cf852937634ca9878671166f710c74
Martin <escorpmartin97@gmail.com>
2021-10-20 19:53:13 -0300

    [coloca condiçao de tabela de peso]

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/imc/index.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/imc/index.js
@@ -4,6 +4,4 @@ const calculoImc = require('./imc');
 const peso = readline.questionFloat(`Qual seu peso? \n`);
 const altura = readline.questionFloat("Qual sua altura? \n");
 
-const imc = calculoImc(peso, altura);
-
-console.log(`Teu imc es: ${imc}`);
+calculoImc(peso, altura);
