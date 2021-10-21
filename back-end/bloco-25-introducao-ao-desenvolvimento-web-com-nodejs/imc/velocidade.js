73d111856925f1b0b57fa42f89dd2583b51f7a48
Martin <escorpmartin97@gmail.com>
2021-10-20 22:15:07 -0300

    [unidades de medida]

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/imc/velocidade.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/imc/velocidade.js
@@ -1,8 +1,8 @@
 const readline = require('readline-sync');
 
 const vMedia = () => {
-  const distancia = readline.questionFloat(`Insira a distancia: `);
-  const tempo = readline.questionFloat(`Insira o tempo: `);
+  const distancia = readline.questionInt(`Insira a distancia(m): `);
+  const tempo = readline.questionInt(`Insira o tempo(s): `);
   const media = (distancia / tempo).toFixed(2);
   return console.log(`A velocidade media é: ${media} m/s`);
 };
