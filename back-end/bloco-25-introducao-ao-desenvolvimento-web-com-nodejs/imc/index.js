505b55ac8724457dfbb1b454de7f68646c90afaf
Martin <escorpmartin97@gmail.com>
2021-10-21 00:17:17 -0300

    [finaliza menu]

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/imc/index.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/imc/index.js
@@ -1,1 +1,18 @@
-console.log("sou index");
+const readline = require('readline-sync');
+
+const scripts = [
+  { name: "Calcular IMC", script: "./imc.js" },
+  { name: "Calcular velocidade média", script: "./velocidade" },
+  { name: "Jogo de sorteio", script: "./sorteio" }
+];
+ const menu = scripts.map(({ name }, index) => `\n${index + 1} - ${name}\n`);
+ menu.unshift('Escolha um número para executar o script correspondente \n');
+
+  // menu = menu.split('\n');
+
+  const selectInput = readline.questionInt(menu) -1;
+  const script = scripts[selectInput];
+
+  if (!script) return console.log("Número invalido");
+
+require(script.script);
