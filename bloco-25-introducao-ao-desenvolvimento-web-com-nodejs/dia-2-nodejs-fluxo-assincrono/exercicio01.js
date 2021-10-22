e0b66fe44ac3af18bf2bf9d2cb36ee3f3d22cced
Martin <escorpmartin97@gmail.com>
2021-10-22 19:27:23 -0300

    [1.3 operacao matematica]

--- bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio01.js
+++ bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio01.js
@@ -2,6 +2,8 @@ const typeNumber = typeof a != 'number' || typeof b != 'number' || typeof c != '
 
 const functionMath = async (a, b, c) => {
   const promise = new Promise((resolve, reject) => {
-    if(typeNumber) reject(new Error("Informe apenas números"))
+    if(typeNumber) reject(new Error("Informe apenas números"));
+    const result = ((a + b) * c);
+    
   });
 }
