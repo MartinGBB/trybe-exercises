d39d454aec4ddb48366bad1a79c65b263ef7b32d
Martin <escorpmartin97@gmail.com>
2021-10-23 15:36:00 -0300

    [busca personagens por id]

--- bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio05.js
+++ bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio05.js
@@ -5,3 +5,16 @@ fs.readFile('./simpsons.json', 'utf-8')
   return JSON.parse(resolve);
 })
 .then((readData) => readData.map(({ id, name }) => console.log(`${id} - ${name}`)));
+
+// Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo "id não encontrado".
+
+const dadosPersonagem = async (id) => {
+  const resolve = await fs.readFile('./simpsons.json', 'utf-8')
+  .then((result) => JSON.parse(result));
+  const simpsons = resolve.find((simpson) => simpson.id === id);
+  if(!simpsons) {
+    throw new Error('id não encontrado');
+  };
+  return console.log(simpsons);
+};
+dadosPersonagem('5');
