166aad3839992c9cbb2d646e0ba490b0dcb343f9
Martin <escorpmartin97@gmail.com>
2021-10-27 19:04:25 -0300

    [Crie uma rota POST /greetings]

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/exercicios de pratica/index.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/exercicios de pratica/index.js
@@ -12,4 +12,10 @@ app.listen('3001', () => console.log('App ouvindo na porta 3001'));
 app.post('/hello', (req, res) => {
   const { name } = req.body;
   res.status(200).json({ message: `name: ${name}` })
-})
+});
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if (age <= 17) return res.status(401).json({ message: "Unauthorized" });
+  res.status(200).json({ message: `Hello, ${name}`})
+});
