5fdd31a0284b40b27f1e33a4c8b05401ffa7ca6b
Martin <escorpmartin97@gmail.com>
2021-10-27 19:17:47 -0300

    [Crie uma rota PUT /users/:name/:age]

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/exercicios de pratica/index.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/exercicios de pratica/index.js
@@ -7,8 +7,6 @@ app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
 });
 
-app.listen('3001', () => console.log('App ouvindo na porta 3001'));
-
 app.post('/hello', (req, res) => {
   const { name } = req.body;
   res.status(200).json({ message: `name: ${name}` })
@@ -19,3 +17,10 @@ app.post('/greetings', (req, res) => {
   if (age <= 17) return res.status(401).json({ message: "Unauthorized" });
   res.status(200).json({ message: `Hello, ${name}`})
 });
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  res.status(201).json({ message: `Seu nome é ${name}, e você tem ${age} anos de idade` })
+});
+
+app.listen('3001', () => console.log('App ouvindo na porta 3001'));
