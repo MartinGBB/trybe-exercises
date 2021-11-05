f741a4c33667acef5c4146715774d925f49d9c7d
Martin <escorpmartin97@gmail.com>
2021-11-05 20:28:54 -0300

    [exercicio 4]

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/index.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const register = require('./routes/users');
 const price = require('./routes/btc');
 const posts = require('./routes/posts');
+const teams = require('./routes/teams');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,5 +12,6 @@ const PORT = 3000;
 app.use('/user', register);
 app.use('/btc', price);
 app.use('/posts', posts);
+app.use('/teams', teams);
 
 app.listen(PORT, () => console.log(`Porta: ${PORT}`));
