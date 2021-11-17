cf1d881505062625016e80a63f0016f22a167ad9
Martin <escorpmartin97@gmail.com>
2021-11-17 17:25:12 -0300

    [agora com mongo]

---
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example com mongo/models/Books.js
@@ -0,0 +1,52 @@
+const connection = require('./connection');
+const Author = require('./Author');
+
+const getAll = async () => {
+  const [books] = await connection.execute(
+    'SELECT * FROM books;',
+  );
+  return books.map(({
+    id,
+    title,
+    author_id
+  }) => ({
+    id,
+    title,
+    authorId: author_id,
+  }));
+};
+
+const findById = async (author_id) => {
+  const query = 'SELECT * FROM books WHERE author_id = ?;';
+  const [book] = await connection.execute(query, [author_id]);
+
+  return book.map(({
+    id,
+    title,
+    author_id
+  }) => ({
+    id,
+    title,
+    authorId: author_id,
+  }));
+};
+
+const isValid = async (title, authorId) => {
+  const MIN_LENGTH_TITLE = 3;
+  if (!title || typeof title !== 'string' || title.length < MIN_LENGTH_TITLE) return false;
+  if (!authorId || typeof authorId !== 'number' || !(await Author.findById(authorId))) return false;
+  return true;
+};
+
+const create = async (title, authorId) => {
+  const query = 'INSERT INTO books (title, authorId) VALUES (?, ?)';
+  const books = await connection.execute(query, [title, authorId]);
+  return books;
+};
+
+module.exports = {
+  getAll,
+  findById,
+  isValid,
+  create,
+};
