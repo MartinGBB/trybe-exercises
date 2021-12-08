d9c72a996fd0f873453cb76d49375f5b68227494
Martin <escorpmartin97@gmail.com>
2021-12-08 02:34:20 -0300

    cria e envia variavel de ambiente

--- back-end/bloco-28-deployment/dia-1-infraestrutura-deploy-com-heroku/exercices-course/index.js
+++ back-end/bloco-28-deployment/dia-1-infraestrutura-deploy-com-heroku/exercices-course/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+const MESSAGE = process.env.MESSAGE || 'esta vivo!!'
 
 const app = express();
 
 app.get('/', (_req, res) => {
-  res.status(200).send('Está vivo!!')
-})
+  res.status(200).send(MESSAGE);
+});
 
 app.listen(PORT, () => console.log(`ouvindo na porta ${PORT}`))
