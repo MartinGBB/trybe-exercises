2d4f0c6ddf8c5415e1e2546ffad38eacf9e8d019
Martin <escorpmartin97@gmail.com>
2021-12-16 19:48:35 -0300

    mais um exercicio

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/Parte-2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/Parte-2.js
@@ -2,21 +2,39 @@
 const factorial = (numero) => {
     let contador = 0;
     let mult = 1;
-    
-        if (numero === 1 || numero === 0) {
+
+    if (numero === 1 || numero === 0) {
         return 1;
-        }
-        else if (numero >= 2) {
-            for (let index = 0; index < numero; index += 1) {
+    } else if (numero >= 2) {
+        for (let index = 0; index < numero; index += 1) {
             contador += 1;
-            mult *= contador 
-            }
-            return mult;
+            mult *= contador
         }
-        return 'error';
+        return mult;
     }
-    console.log(factorial(4));
+    return 'error';
+}
+console.log(factorial(4));
 
 //tente fazer o mesmo exercício de forma recursiva.
-    const factorial2 = (num) =>  num > 1 ? num * factorial2(num - 1) : 1 
-    console.log(factorial2(4));
+const factorial2 = (num) => num > 1 ? num * factorial2(num - 1) : 1
+console.log(factorial2(4));
+
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+const longestWord = (palavra) => {
+    let frase = palavra.split(' ');
+    let tamanhoPalavra = 0;
+    let palavraMaior = '';
+
+    for (const test of frase) {
+
+        if (test.length > tamanhoPalavra) {
+            tamanhoPalavra = test.length;
+            palavraMaior = test;
+        }
+    }
+    return palavraMaior;
+}
+
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu")) // retorna 'aconteceu'    
