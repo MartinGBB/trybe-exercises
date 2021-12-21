5b3d67c8a8a0db792d8378558cff77234a8b854a
Martin <escorpmartin97@gmail.com>
2021-12-21 16:05:39 -0300

    exercicio 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio_1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio_1.js
@@ -63,14 +63,14 @@ const books = [
   
   // 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
 
-  const expectedResult = [
-    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-    'Fundação - Ficção Científica - Isaac Asimov',
-    'Duna - Ficção Científica - Frank Herbert',
-    'A Coisa - Terror - Stephen King',
-    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
-  ];
+  // const expectedResult = [
+  //   'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  //   'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  //   'Fundação - Ficção Científica - Isaac Asimov',
+  //   'Duna - Ficção Científica - Frank Herbert',
+  //   'A Coisa - Terror - Stephen King',
+  //   'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  // ];
   
   function formatedBookNames(books) {
       const infoLivro = books.map(({ name, genre, author }) => {
@@ -82,33 +82,39 @@ const books = [
 
   // 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
 
-  const expectedResult = [
-    {
-      age: 31,
-      author: 'Isaac Asimov',
-    },
-    {
-      age: 38,
-      author: 'H. P. Lovecraft',
-    },
-    {
-      age: 39,
-      author: 'Stephen King',
-    },
-    {
-      age: 43,
-      author: 'George R. R. Martin',
-    },
-    {
-      age: 45,
-      author: 'Frank Herbert',
-    },
-    {
-      age: 62,
-      author: 'J. R. R. Tolkien',
-    },
-  ];
+  // const expectedResult = [
+  //   {
+  //     age: 31,
+  //     author: 'Isaac Asimov',
+  //   },
+  //   {
+  //     age: 38,
+  //     author: 'H. P. Lovecraft',
+  //   },
+  //   {
+  //     age: 39,
+  //     author: 'Stephen King',
+  //   },
+  //   {
+  //     age: 43,
+  //     author: 'George R. R. Martin',
+  //   },
+  //   {
+  //     age: 45,
+  //     author: 'Frank Herbert',
+  //   },
+  //   {
+  //     age: 62,
+  //     author: 'J. R. R. Tolkien',
+  //   },
+  // ];
   
-  function nameAndAge() {
-    // escreva seu código aqui
-  }
+  function nameAndAge(books) {
+    const author = books.map(({ author: { name, birthYear }, releaseYear}) => {
+      const ageAuthor = (releaseYear - birthYear);
+      return { age: ageAuthor, author: name };
+    }).sort((a, b) => a.age - b.age);
+    return author;
+  };
+  console.log(nameAndAge(books));
+  
