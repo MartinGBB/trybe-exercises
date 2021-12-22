6a6c5d0df2fd4c5e10ebd20dfbac2cf62fb742cd
Martin <escorpmartin97@gmail.com>
2021-12-22 11:07:30 -0300

    exercicio 4 - desafios

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/script2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/script2.js
@@ -89,3 +89,25 @@ function averageAge() {
   return mediaDeIdade / quantidadeLivros;
 }
 console.log(averageAge());
+
+// 4 - Encontre o livro com o maior nome.
+
+// const expectedResult = {
+//   id: 1,
+//   name: 'As Crônicas de Gelo e Fogo',
+//   genre: 'Fantasia',
+//   author: {
+//     name: 'George R. R. Martin',
+//     birthYear: 1948,
+//   },
+//   releaseYear: 1991,
+// }
+
+function longestNamedBook() {
+  const maiorNome = books.reduce((acc, book) => {
+    return acc.name.length > book.name.length ? acc : book;
+  });
+  return maiorNome;
+};
+
+console.log(longestNamedBook());
