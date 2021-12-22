d9262770d7e28522bfd9666d1b3c4593235e4a49
Martin <escorpmartin97@gmail.com>
2021-12-22 14:48:18 -0300

    adiciona exercicio de default parameters

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise_1_aula.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise_1_aula.js
@@ -68,3 +68,12 @@ console.log(getNationality(person));
 const getPosition = (latitude, longitude) => (latitude, longitude);
 
 console.log(getPosition(-19.8157, -43.9542));
+
+
+// Default Parameters
+
+// 1 - escreva uma função multiply que multiplique dois números passados como argumentos. Atribua como default o valor 1 caso não seja passado nenhum valor como segundo parâmetro.
+
+const multiply = (number, value = 1) => (number * value);
+
+console.log(multiply(8));
