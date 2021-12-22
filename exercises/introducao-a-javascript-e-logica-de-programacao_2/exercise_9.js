1e333fd3abd34ffb32d775b654c928e2013784de
Martin <escorpmartin97@gmail.com>
2021-12-22 16:42:27 -0300

    corrige pastas

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_9.js
+++
@@ -1,7 +0,0 @@
-//9)Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-let conta = 0;
-for (let contador of arr) {
-    contador = contador /2;
-    console.log(contador);
-};
