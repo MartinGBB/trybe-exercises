9f357d261c35944b241990e71a384d7341d3974d
Martin <escorpmartin97@gmail.com>
2021-12-22 13:55:26 -0300

    muda pastas e adiciona exercicio 1 da aula

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise_1_aula.js
@@ -0,0 +1,13 @@
+// 1 - vamos fazer uma salada de frutas com itens adicionais que você desejar. Faça uma função chamada fruitSalad passando como parâmetro specialFruit e additionalItens , faça a função retornar uma lista única contendo todos os itens da nossa salada de frutas usando o spread .
+
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['banana', 'maça', 'morango'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['ceriais', 'castanha', 'aveia'];
+
+const fruitSalad = (fruit, additional) => {
+  return [...fruit, ...additional];
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
