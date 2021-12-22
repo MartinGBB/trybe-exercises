9e842e2e5a3adb4fc0deb7d8f953c5ddbf4c5886
Martin <escorpmartin97@gmail.com>
2021-12-22 14:42:14 -0300

    resolve exercicio de Property shorthand

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise_1_aula.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise_1_aula.js
@@ -65,8 +65,6 @@ console.log(getNationality(person));
 
 // 1 -  altere a função getPosition utilizando a property shorthand.
 
-const getPosition = (latitude, longitude) => ({
-  latitude: latitude,
-  longitude: longitude});
+const getPosition = (latitude, longitude) => (latitude, longitude);
 
 console.log(getPosition(-19.8157, -43.9542));
