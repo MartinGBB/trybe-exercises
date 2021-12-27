4d69c0c211d863a5dd1412dfe2ac913ac3532ef3
Martin <escorpmartin97@gmail.com>
2021-12-27 19:27:07 -0300

    adiciona 3 botoes que imprimam messagens diferentes

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-button/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-button/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import './App.css';
 
+const handleClickBtn1 = () => (
+  console.log('btn-1')
+);
+
+const handleClickBtn2 = () => (
+  console.log('btn-2')
+);
+
+const handleClickBtn3 = () => (
+  console.log('btn-3')
+);
+
 class App extends React.Component {
   render() {
 
     return (
     <div>
-      
+      <button onClick={ handleClickBtn1 }>botão-1</button>
+      <button onClick={ handleClickBtn2 }>botão-2</button>
+      <button onClick={ handleClickBtn3 }>botão-3</button>
     </div>
   );
   }
