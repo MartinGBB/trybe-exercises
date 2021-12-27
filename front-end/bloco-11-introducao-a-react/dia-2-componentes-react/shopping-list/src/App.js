be032733883e2a2a5afda01a8c9bbbe77be0139b
Martin <escorpmartin97@gmail.com>
2021-12-27 18:21:01 -0300

    adiciona lista de compras

--- front-end/bloco-11-introducao-a-react/dia-2-componentes-react/shopping-list/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia-2-componentes-react/shopping-list/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  render() {
+    const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lista de compras</h1>
     </div>
   );
+  }
 }
 
 export default App;
