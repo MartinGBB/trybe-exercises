a618457d3814a97d07911ce29a81d6429c752a9c
Martin <escorpmartin97@gmail.com>
2021-12-27 19:12:35 -0300

    substitue exercicio

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-button/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-button/src/App.js
@@ -1,49 +1,25 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-
-class App extends React.Component {
-
-  constructor () {
-    super()
-    this.handleClick = this.handleClick.bind(this);
-    this.handleClick2 = this.handleClick2.bind(this);
-    this.handleClick3 = this.handleClick3.bind(this);
-
-    this.state = {
-      clickBtn1: 0,
-      clickBtn2: 0,
-      clickBtn3: 0,
-    };
-  };
-
-  handleClick() {
-    this.setState((prevenState) => ({
-      clickBtn1: prevenState.clickBtn1 + 1,
-    }));
-  };
-  
-  handleClick2() {
-    this.setState((prevenState) => ({
-      clickBtn2: prevenState.clickBtn2 + 1,
-    }))
-  };
-  
-  handleClick3() {
-    this.setState((prevenState) => ({
-      clickBtn3: prevenState.clickBtn3 + 1,
-    }))
-  };
-
-  render() {
-    return (
-      <div>
-      <button onClick={ this.handleClick }>Primer buttom | NumClick = { this.state.clickBtn1 }</button>
-      <button onClick={ this.handleClick2 }>Segundo buttom | NumClick= { this.state.clickBtn2 }</button>
-      <button onClick={ this.handleClick3 }>Terceiro buttom | NumClick= { this.state.clickBtn3 }</button>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
