a3e90da28fbec0ac1a68bc844cac6bfc4d98cf2f
Martin <escorpmartin97@gmail.com>
2021-12-27 16:21:01 -0300

    verifica que o repositorio iss-location esta

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-3-jest-testes-assincronos/githubapi.test.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-3-jest-testes-assincronos/githubapi.test.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const { expect } = require('@jest/globals');
 
-//1 - O código abaixo busca no GitHub de um usuário, de acordo com a URL, seus repositórios e retorna uma lista como resultado. Dada a URL 'https://api.github.com/orgs/tryber/repos' , faça um teste que verifique que os repositórios 'sd-01-week4-5-project-todo-list' e 'sd-01-week4-5-project-meme-generator' não se encontram nessa lista e que os repositorios "linter-template" e "iss-location.
+//1 - O código abaixo busca no GitHub de um usuário, de acordo com a URL, seus repositórios e retorna uma lista como resultado. Dada a URL 'https://api.github.com/orgs/tryber/repos' , faça um teste que verifique que os repositórios 'sd-01-week4-5-project-todo-list' e 'sd-01-week4-5-project-meme-generator' não se encontram nessa lista e que os repositorios "linter-template" e "iss-location".
 
 
 const getRepos = (url) => (
@@ -23,6 +23,6 @@ const getRepos = (url) => (
     test('verifica que estão os repositorios', async () => {
       const repositories = await getRepos(url);
       expect(repositories).toContain('linter-template');
-      
-    })
-  })
+      expect(repositories).toContain('iss-location');
+    });
+  });
