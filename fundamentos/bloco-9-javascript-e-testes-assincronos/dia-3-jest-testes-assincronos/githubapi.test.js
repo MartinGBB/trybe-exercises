6f18c613a34f243107791af7b587e17c5a833513
Martin <escorpmartin97@gmail.com>
2021-12-27 16:11:37 -0300

    verifica que existe repository

---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-3-jest-testes-assincronos/githubapi.test.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+const { expect } = require('@jest/globals');
+
+//1 - O código abaixo busca no GitHub de um usuário, de acordo com a URL, seus repositórios e retorna uma lista como resultado. Dada a URL 'https://api.github.com/orgs/tryber/repos' , faça um teste que verifique que os repositórios 'sd-01-week4-5-project-todo-list' e 'sd-01-week4-5-project-meme-generator' se encontram nessa lista.
+
+
+const getRepos = (url) => (
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => data.map((repo) => repo.name))
+  );
+  
+  
+  
+  test('Aparece repositorios', async () => {
+    const url = 'https://api.github.com/orgs/tryber/repos';
+    const repositories = await getRepos(url);
+    expect(repositories).toContain('sd-01-week4-5-project-todo-list');
+  })
+  
+//   const searchRepositories = async () => {
+//   const repositories = await getRepos(url);
+//   const matchRepositories = repositories.filter((repository) => repository === )
+// }
