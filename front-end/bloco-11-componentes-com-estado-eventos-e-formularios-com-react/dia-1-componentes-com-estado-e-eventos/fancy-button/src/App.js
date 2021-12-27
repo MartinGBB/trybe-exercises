e5c3195282fd0c748d32ece41efe2b3b94dc30be
Martin <escorpmartin97@gmail.com>
2021-12-27 19:45:26 -0300

    coloca funciones dentro do componente

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-button/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-button/src/App.js
@@ -1,29 +1,35 @@
 import React from 'react'
 import './App.css';
 
-const handleClickBtn1 = () => (
-  console.log('btn-1')
-);
-
-const handleClickBtn2 = () => (
-  console.log('btn-2')
-);
-
-const handleClickBtn3 = () => (
-  console.log('btn-3')
-);
-
 class App extends React.Component {
-  render() {
+  constructor() {
+    super()
+    this.handleClickBtn1 = this.handleClickBtn1.bind(this);
+    this.handleClickBtn2 = this.handleClickBtn2.bind(this);
+    this.handleClickBtn2 = this.handleClickBtn2.bind(this);
+  };
 
+  handleClickBtn1 = () => (
+    console.log('btn-1')
+  );
+  
+  handleClickBtn2 = () => (
+    console.log('btn-2')
+  );
+  
+  handleClickBtn3 = () => (
+    console.log('btn-3')
+  );
+
+  render() {
     return (
     <div>
-      <button onClick={ handleClickBtn1 }>botão-1</button>
-      <button onClick={ handleClickBtn2 }>botão-2</button>
-      <button onClick={ handleClickBtn3 }>botão-3</button>
+      <button onClick={ this.handleClickBtn1 }>botão-1</button>
+      <button onClick={ this.handleClickBtn2 }>botão-2</button>
+      <button onClick={ this.handleClickBtn3 }>botão-3</button>
     </div>
-  );
-  }
-}
+    );
+  };
+};
 
 export default App;
