aa5abd40321e5b098fd146a767c597f22e9b36b0
Martin <escorpmartin97@gmail.com>
2021-12-27 23:20:56 -0300

    cria formulario

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/form-exercicio-conteudo/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/form-exercicio-conteudo/src/App.js
@@ -2,15 +2,47 @@ import React from 'react'
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
 
-  }
 
   render() {
 
     return (
       <div className="App">
-        
+        <h1>Formulario de pratica</h1>
+        <form>
+
+          <label>
+          Seleccione estado
+            <select>
+            <option>seleccione</option>
+              <option>Minas Gerais</option>
+              <option>Santa Catarina</option>
+            </select>
+          </label>
+
+          <label>
+            nome
+            <input
+              name="nome"
+              type="text"
+            />
+          </label>
+
+          <label>
+            vou comparecer
+            <input
+            type="checkbox"
+            name="vaiComparecer"
+            />
+          </label>
+
+          <label>
+            de uma descripção de você:
+            <textarea
+            name="description"
+            />
+          </label>
+        </form>
       </div>
     );
   }
