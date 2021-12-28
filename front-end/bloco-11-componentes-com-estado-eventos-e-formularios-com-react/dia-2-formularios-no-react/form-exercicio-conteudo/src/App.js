f7f07cc4a71ac6abec1b68982a02c974aa423ab5
Martin <escorpmartin97@gmail.com>
2021-12-28 10:16:33 -0300

    separa componente form de app

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/form-exercicio-conteudo/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/form-exercicio-conteudo/src/App.js
@@ -1,78 +1,12 @@
 import React from 'react'
 import './App.css';
+import Form  from './Form';
 
 class App extends React.Component {
-  constructor() {
-    super()
-
-    this.handleChange = this.handleChange.bind(this);
-
-    this.state = {
-      estado: 'Seleccione',
-      nome: '',
-      vaiComparecer: false,
-      description: '',
-    };
-  };
-  
-    handleChange = ({ target }) => {
-      const { name } = target
-      const value = target.type === 'checkbox' ? target.checked : target.value
-      this.setState({
-        [name]: value,
-      });
-    };
-
   render() {
-    const { estado, name, vaiComparecer, description } = this.state;
-
     return (
       <div className="App">
-        <h1>Formulario de pratica</h1>
-        <form>
-
-          <label>
-          Seleccione estado
-            <select
-            name="estado"
-            value={ estado }
-            onChange={ this.handleChange }
-            >
-            <option>seleccione</option>
-              <option>Minas Gerais</option>
-              <option>Santa Catarina</option>
-            </select>
-          </label>
-
-          <label>
-            nome
-            <input
-              name="nome"
-              type="text"
-              value={ name }
-              onChange={ this.handleChange }
-            />
-          </label>
-
-          <label>
-            vou comparecer
-            <input
-            type="checkbox"
-            name="vaiComparecer"
-            value={ vaiComparecer }
-            onChange={ this.handleChange }
-            />
-          </label>
-
-          <label>
-            de uma descripção de você:
-            <textarea
-            name="description"
-            value={ description }
-            onChange={ this.handleChange }
-            />
-          </label>
-        </form>
+        <Form />
       </div>
     );
   }
