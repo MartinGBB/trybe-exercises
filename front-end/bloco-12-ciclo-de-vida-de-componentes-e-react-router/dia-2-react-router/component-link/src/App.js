20af10a22a2f25d58647df6669ea92edc21555c4
Martin <escorpmartin97@gmail.com>
2021-12-29 16:25:04 -0300

    adicione uma rota que mapeie o caminho de URL "/users" para o componente Users

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/component-link/src/App.js
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/component-link/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route } from  'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Users from './Users';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <div className="App">
           <Route exact path='/' component={ Home } />
           <Route path='/about' component={ About } />
+          <Route path='/users' component={ Users } />
         </div>
       </BrowserRouter>
     );
---
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/component-link/src/Users.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <p>Component Users</p>
+      </div>
+    );
+  }
+}
+
+export default Users;
