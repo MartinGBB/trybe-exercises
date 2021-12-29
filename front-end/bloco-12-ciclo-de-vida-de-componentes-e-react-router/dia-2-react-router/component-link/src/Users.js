d56bd814c76d6efd60c8aa17269ddd17be0611c6
Martin <escorpmartin97@gmail.com>
2021-12-29 17:20:10 -0300

    recupera parametro da rota

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/component-link/src/Users.js
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/component-link/src/Users.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 class Users extends Component {
   render() {
     const { greetingsMessage } = this.props
+    const { id } = this.props.match.params;
     return (
       <div>
         <p> { greetingsMessage }, My awesome Users component </p>
+        <p>id: { id }</p>
         <Link to='/'>voltar</Link>
       </div>
     );
