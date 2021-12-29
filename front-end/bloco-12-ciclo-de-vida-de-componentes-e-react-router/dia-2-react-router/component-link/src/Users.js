3333c8d50f8891db76a3eeb6b6ce1abfdeef87ee
Martin <escorpmartin97@gmail.com>
2021-12-29 17:04:08 -0300

    altera users para receber message via props

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/component-link/src/Users.js
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-2-react-router/component-link/src/Users.js
@@ -2,9 +2,11 @@ import { Component } from 'react';
 import { Link } from 'react-router-dom';
 class Users extends Component {
   render() {
+    const { greetingsMessage } = this.props
     return (
       <div>
         <p>Component Users</p>
+        <p> { greetingsMessage }, My awesome Users component </p>
         <Link to='/'>voltar</Link>
       </div>
     );
