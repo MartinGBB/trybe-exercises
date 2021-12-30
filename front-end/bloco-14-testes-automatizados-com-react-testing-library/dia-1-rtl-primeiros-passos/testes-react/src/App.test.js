d827b8c7bc807d33bad73361b2940ac35eeb4160
Martin <escorpmartin97@gmail.com>
2021-12-30 18:16:35 -0300

    adiciona describe

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
@@ -2,39 +2,41 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('input na tela', () => {
-  render(<App />);
-  const renderInput = screen.getByLabelText('Email');
-  expect(renderInput).toBeInTheDocument();
-  expect(renderInput).toHaveProperty('type', 'email');
-});
-
-test('renderiza botao', () => {
-  render(<App />);
-  const button = screen.getByTestId('btn-1');
-  expect(button).toBeInTheDocument();
-  expect(button).toHaveProperty('type', 'button')
-});
-
-test('renderiza dois botoes', () => {
-  render(<App />);
-  const buttons = screen.getAllByRole('button');
-  expect(buttons).toHaveLength(2)
-});
-
-test('verifica que é impreso o email na tela', () => {
-  render(<App />);
-  const EMAIL = "email@email.com";
-
-  const emailPrint = screen.getByTestId('id-email-user');
-  expect(emailPrint).toBeInTheDocument();
-  expect(emailPrint).toHaveTextContent('Valor');
+describe('testa tela principal', () => {
+  it('input na tela', () => {
+    render(<App />);
+    const renderInput = screen.getByLabelText('Email');
+    expect(renderInput).toBeInTheDocument();
+    expect(renderInput).toHaveProperty('type', 'email');
+  });
   
-  const emailInput = screen.getByLabelText('Email');
-  const button = screen.getByTestId('btn-1')
-  userEvent.type(emailInput, EMAIL);
-  userEvent.click(button);
+  it('renderiza botao', () => {
+    render(<App />);
+    const button = screen.getByTestId('btn-1');
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveProperty('type', 'button')
+  });
   
-  expect(emailInput).toHaveValue('')
-  expect(emailPrint).toHaveTextContent(`Valor: ${EMAIL}`)
-})
+  it('renderiza dois botoes', () => {
+    render(<App />);
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(2)
+  });
+  
+  it('verifica que é impreso o email na tela', () => {
+    render(<App />);
+    const EMAIL = "email@email.com";
+    
+    const emailPrint = screen.getByTestId('id-email-user');
+    expect(emailPrint).toBeInTheDocument();
+    expect(emailPrint).toHaveTextContent('Valor');
+    
+    const emailInput = screen.getByLabelText('Email');
+    const button = screen.getByTestId('btn-1')
+    userEvent.type(emailInput, EMAIL);
+    userEvent.click(button);
+    
+    expect(emailInput).toHaveValue('')
+    expect(emailPrint).toHaveTextContent(`Valor: ${EMAIL}`)
+  })
+});
