b5c666616dabc47efcb7f7154ef57236b667f029
Martin <escorpmartin97@gmail.com>
2021-12-30 15:39:57 -0300

    adiciona teste a input

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const renderInput = screen.getByLabelText('Email');
+  expect(renderInput).toBeInTheDocument();
+  expect(renderInput).toHaveProperty('type', 'email')
 });
