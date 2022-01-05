75faff26d4361f9db1788692cfa067b72413ffa8
Martin <escorpmartin97@gmail.com>
2022-01-05 01:17:57 -0300

    busca por id

---
+++ back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/api-library/controllers/books/get.js
@@ -0,0 +1,13 @@
+const { Book } = require('../../models');
+
+module.exports = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findByPk(id);
+    if (!book) return res.status(404).json({ message: 'Livro não encontrado' });
+    res.status(200).json(book);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+};
--- back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/api-library/controllers/books/router.js
+++ back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/api-library/controllers/books/router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const list = require('./list');
+const get = require('./get');
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/', list);
+router.get('/:id', get);
 
 module.exports = router;
