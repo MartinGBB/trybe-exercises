14bf26390b4d2b1d8a59d748493d78a7b15040a6
Martin <escorpmartin97@gmail.com>
2022-01-05 01:29:34 -0300

    actualiza novo usuario

--- back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/api-library/controllers/books/router.js
+++ back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/api-library/controllers/books/router.js
@@ -2,11 +2,13 @@ const express = require('express');
 const list = require('./list');
 const get = require('./get');
 const create = require('./create');
+const update = require('./update');
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/', list);
 router.get('/:id', get);
 router.post('/', create);
+router.post('/:id', update);
 
 module.exports = router;
