4dd2859cec0a759600abc03b1751cbd2d030d07d
Martin <escorpmartin97@gmail.com>
2022-01-05 01:10:34 -0300

    rota list

--- back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/api-library/controllers/books/list.js
+++ back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/api-library/controllers/books/list.js
@@ -1,8 +1,11 @@
-module.exports = (req, res) => {
-  try {
-    
-  } catch (err) {
-    console.log(err);
+const { Book } = require('../../models');
 
-  }
+module.exports = async (req, res) => {
+  try {
+    const books = await Book.findAll();
+    res.status(200).json(books);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
 };
