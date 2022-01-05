9501189468aba8df88ec40c8e2f54daf6d48828b
Martin <escorpmartin97@gmail.com>
2022-01-05 01:23:06 -0300

    adiciona rota de criar

--- back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/api-library/controllers/books/router.js
+++ back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/api-library/controllers/books/router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const list = require('./list');
 const get = require('./get');
+const create = require('./create');
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/', list);
 router.get('/:id', get);
+router.post('/', create);
 
 module.exports = router;
