9c89e9afb7e5dbb450b7b1fc4ffaf0e3a2d6c941
Martin <escorpmartin97@gmail.com>
2022-01-07 02:05:38 -0300

    exercicio final

--- back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/models/products.js
+++
@@ -1,9 +0,0 @@
-const Products = (sequelize, DataTypes) => {
-  const Products = sequelize.define("Products", {
-    product: DataTypes.STRING
-  });
-
-  return Products;
-};
-
-module.exports = Products;
