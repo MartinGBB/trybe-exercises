b00815597f02ed1bc8aed8acb4208ba7ddf7f1fd
Martin <escorpmartin97@gmail.com>
2022-01-17 23:09:49 -0300

    termina ultimas modifficacoes

--- back-end/bloco-30-sockets/dia-1-arquitetura-de-software-camada-de-view/mcv-example/index.js
+++ back-end/bloco-30-sockets/dia-1-arquitetura-de-software-camada-de-view/mcv-example/index.js
@@ -4,7 +4,13 @@ const authorController = require('./controllers/authorController');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.set('views', './views');
+
 app.get('/authors', authorController.listAuthors);
+app.get('/authors/:id', authorController.showAuthor);
+
 
 const PORT = process.env.PORT || 3000;
 
