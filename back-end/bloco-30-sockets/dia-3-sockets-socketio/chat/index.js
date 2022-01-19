95f293e12da18e18b01a4b4b6a1cf31e9d8eccac
Martin <escorpmartin97@gmail.com>
2022-01-19 16:53:29 -0300

    inicia app e configura pacotes

---
+++ back-end/bloco-30-sockets/dia-3-sockets-socketio/chat/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`usuario connectado com id: ${socket.id}`)
+});
+
+
+http.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
