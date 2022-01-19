97e166d117e9e88bd6068aa149a8026c0d3245b2
Martin <escorpmartin97@gmail.com>
2022-01-19 16:26:16 -0300

    refatora

--- back-end/bloco-30-sockets/dia-3-sockets-socketio/pratica/index.js
+++ back-end/bloco-30-sockets/dia-3-sockets-socketio/pratica/index.js
@@ -3,25 +3,15 @@ const app = express();
 const PORT = 3000;
 const http = require('http').createServer(app);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
 const io = require('socket.io')(http, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
   }});
 
-  io.on('connection', (socket) => {
-    console.log(`id do usuario: ${socket.id}`)
-      socket.emit('ola', 'Que bom que você chegou aqui! Fica mais um cadin, vai ter bolo :)')
-    
-      socket.on('ping', () => {
-        console.log(`${socket.id} emitiu um ping`);
-        io.emit('pong', `${socket.id} enviou um ping`)
-      });
-  });
+  app.use(express.static(__dirname + '/public'));
+
+require('./sockets/ping')(io);
 
 http.listen(PORT, () => {
   console.log(`Servidor ouvindo na porta ${PORT}`);
