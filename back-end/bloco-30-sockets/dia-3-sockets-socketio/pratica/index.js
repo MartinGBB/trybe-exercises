a37d755b4bba4460807722c51d0a0c36211c9d2f
Martin <escorpmartin97@gmail.com>
2022-01-19 14:47:05 -0300

    configura socket

--- back-end/bloco-30-sockets/dia-3-sockets-socketio/pratica/index.js
+++ back-end/bloco-30-sockets/dia-3-sockets-socketio/pratica/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 const http = require('http').createServer(app);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, () => {
-  console.log('Servidor ouvindo na porta 3000');
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  }});
+
+  io.on('connection', (socket) => {
+    console.log(`id do usuario ${socket.id}`)
+  })
+
+http.listen(PORT, () => {
+  console.log(`Servidor ouvindo na porta ${PORT}`);
 });
