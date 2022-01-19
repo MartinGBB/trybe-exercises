a35ed782fb9c7719a044b042173dc0bed335690f
Martin <escorpmartin97@gmail.com>
2022-01-19 10:27:44 -0300

    envia mensage de cliente para servidor

--- back-end/bloco-30-sockets/dia-2-sockets-tcpudp-net/server.js
+++ back-end/bloco-30-sockets/dia-2-sockets-tcpudp-net/server.js
@@ -1,15 +1,19 @@
 /* Importando o pacote NET, responsável pela implementação dos sockets no Node. */
 const net = require('net');
-
+const connections = [];
 /* Criando o servidor com o método 'createServer', onde recebe uma conexao na qual são expostos os eventos que podemos manipular no nosso servidor. */
 const server = net.createServer((connection) => {
+  connections.push(connection)
   // console.log('Cliente conectado');
   
-  // /* Assim como um evento normal do Node.js, o método ".on()" escuta um evento em específico e, quando ele é ativado, nossa função de callback é chamada. */
-  // connection.on('end', () => {
-  //   console.log('Cliente desconectado');
-  // });
-  // /* Nessa conexão que foi aberta, podemos fazer várias coisas. Uma delas é escrever/devolver uma mensagem para o cliente. */
+  connection.on('end', () => {
+    console.log('Cliente desconectado');
+  });
+
+  connection.on('data', (data) => {
+    console.log(`o client disse: ${data}`);
+  });
+  /* Nessa conexão que foi aberta, podemos fazer várias coisas. Uma delas é escrever/devolver uma mensagem para o cliente. */
   // connection.write('Mensagem do servidor!\r\n');
   // connection.pipe(connection);
   server.getConnections((err, count) => (console.log(count ++)));
