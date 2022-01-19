38a82e5d7ea4d0209c54d2ea472fc6aa357f163b
Martin <escorpmartin97@gmail.com>
2022-01-19 17:44:12 -0300

    adiciona css e recebe e envia message

--- back-end/bloco-30-sockets/dia-3-sockets-socketio/chat/public/js/chat.js
+++ back-end/bloco-30-sockets/dia-3-sockets-socketio/chat/public/js/chat.js
@@ -2,11 +2,19 @@ const socket = window.io();
 
 const form = document.querySelector('form');
 const ul = document.querySelector('#messages');
-const input = document.querySelector('#messageInput');
+const messageInput = document.querySelector('#messageInput');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  socket.emit('clientMessage', input.value);
-  input.value = '';
+  socket.emit('clientMessage', messageInput.value);
+  messageInput.value = '';
   return false;
 });
+
+const createMessage = (message) => {
+  const li = document.createElement('li');
+  li.innerText = message;
+  ul.appendChild(li);
+};
+
+socket.on('serverMessage', (message) => createMessage(message));
--- back-end/bloco-30-sockets/dia-3-sockets-socketio/chat/sockets/chat.js
+++ back-end/bloco-30-sockets/dia-3-sockets-socketio/chat/sockets/chat.js
@@ -1,5 +1,7 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    
-  })
+    socket.on('clientMessage', (message) => {
+      io.emit('serverMessage', message)
+    });
+  });
 };
