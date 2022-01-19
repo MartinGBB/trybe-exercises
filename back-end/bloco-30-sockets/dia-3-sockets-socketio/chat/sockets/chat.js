6fbc5cd32a5a3241c10b0b6385116f81ce72e6d1
Martin <escorpmartin97@gmail.com>
2022-01-19 17:54:52 -0300

    testa enviando uma menssagem para apenas a pessoa que inicia no server

--- back-end/bloco-30-sockets/dia-3-sockets-socketio/chat/sockets/chat.js
+++ back-end/bloco-30-sockets/dia-3-sockets-socketio/chat/sockets/chat.js
@@ -1,5 +1,6 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
+    socket.emit('serverMessage', 'Olá, seja bem vindo ao nosso chat público! Use essa página para conversar a vontade.');
     socket.on('clientMessage', (message) => {
       io.emit('serverMessage', message)
     });
