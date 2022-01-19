55c5882f3305067811a6f29aecd9d2f5384f9aa7
Martin <escorpmartin97@gmail.com>
2022-01-19 09:52:16 -0300

    testando que o servidor mantenha a conexao

--- back-end/bloco-30-sockets/dia-2-sockets-tcpudp-net/server.js
+++ back-end/bloco-30-sockets/dia-2-sockets-tcpudp-net/server.js
@@ -3,15 +3,15 @@ const net = require('net');
 
 /* Criando o servidor com o método 'createServer', onde recebe uma conexao na qual são expostos os eventos que podemos manipular no nosso servidor. */
 const server = net.createServer((connection) => {
-  console.log('Cliente conectado');
+  // console.log('Cliente conectado');
 
-  /* Assim como um evento normal do Node.js, o método ".on()" escuta um evento em específico e, quando ele é ativado, nossa função de callback é chamada. */
-  connection.on('end', () => {
-    console.log('Cliente desconectado');
-  });
-  /* Nessa conexão que foi aberta, podemos fazer várias coisas. Uma delas é escrever/devolver uma mensagem para o cliente. */
-  connection.write('Mensagem do servidor!\r\n');
-  connection.pipe(connection);
+  // /* Assim como um evento normal do Node.js, o método ".on()" escuta um evento em específico e, quando ele é ativado, nossa função de callback é chamada. */
+  // connection.on('end', () => {
+  //   console.log('Cliente desconectado');
+  // });
+  // /* Nessa conexão que foi aberta, podemos fazer várias coisas. Uma delas é escrever/devolver uma mensagem para o cliente. */
+  // connection.write('Mensagem do servidor!\r\n');
+  // connection.pipe(connection);
 });
 
 /* Após termos programado o servidor, é só colocá-lo de pé */
