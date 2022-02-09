681173a4ff33c543dae0594558eee1004c9f3d28
Martin <escorpmartin97@gmail.com>
2022-02-09 16:21:36 -0300

    calcule area of rectangle

--- ciencia-da-computacao/bloco-32-introducao-a-python/dia-1-aprendendo-python/area.py
+++ ciencia-da-computacao/bloco-32-introducao-a-python/dia-1-aprendendo-python/area.py
@@ -5,4 +5,9 @@ def square(side):
     return f"Area of square: {side * side}"
 
 
+def rectangle(length, width):
+    return f"Area of rectangle{length * width}"
+
+
 print(square(5))
+print(rectangle(5, 5))
