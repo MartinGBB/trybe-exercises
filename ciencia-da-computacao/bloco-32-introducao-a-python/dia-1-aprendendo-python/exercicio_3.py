96796f6beb7646d85103adb54606bd0f64af2f07
Martin <escorpmartin97@gmail.com>
2022-02-09 19:39:13 -0300

    corrige comentario

--- ciencia-da-computacao/bloco-32-introducao-a-python/dia-1-aprendendo-python/exercicio_3.py
+++ ciencia-da-computacao/bloco-32-introducao-a-python/dia-1-aprendendo-python/exercicio_3.py
@@ -1,4 +1,5 @@
-#   Faça um programa que, dado um valor n qualquer, tal que n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+#   Faça um programa que, dado um valor n qualquer, tal que n > 1,
+#   imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
 
 
 def frame(n):
