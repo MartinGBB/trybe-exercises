9101e53166ed9cf618c07ae9f7d58ae57c797ae2
Martin <escorpmartin97@gmail.com>
2022-02-10 18:19:16 -0300

    cria arquivo com pokemons filtrados

--- ciencia-da-computacao/bloco-32-introducao-a-python/dia-2-entrada-e-saida-de-dados/get_pokemons.py
+++ ciencia-da-computacao/bloco-32-introducao-a-python/dia-2-entrada-e-saida-de-dados/get_pokemons.py
@@ -2,8 +2,12 @@ import json
 
 with open("pokemons.json") as file:
     pokemons = json.load(file)["results"]
-    pokemons_type_fire = [
+    fire_type_pokemons = [
         pokemon for pokemon in pokemons if "Fire" in pokemon["type"]
     ]
 
-print(pokemons_type_fire[0])
+print(fire_type_pokemons[0])
+
+with open("fire_type_pokemons.txt", mode="w") as fire_pokemons:
+    json_write = json.dumps(fire_type_pokemons)
+    fire_pokemons.write(json_write)
