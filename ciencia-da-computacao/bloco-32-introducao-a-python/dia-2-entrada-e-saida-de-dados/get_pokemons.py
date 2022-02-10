5ba2d3d62a781c0fcfa0014ceafbeb06db9a4273
Martin <escorpmartin97@gmail.com>
2022-02-10 18:24:37 -0300

    utiliza dump no lugar de dumps e separa filtro do with de que abre json

--- ciencia-da-computacao/bloco-32-introducao-a-python/dia-2-entrada-e-saida-de-dados/get_pokemons.py
+++ ciencia-da-computacao/bloco-32-introducao-a-python/dia-2-entrada-e-saida-de-dados/get_pokemons.py
@@ -2,12 +2,10 @@ import json
 
 with open("pokemons.json") as file:
     pokemons = json.load(file)["results"]
-    fire_type_pokemons = [
-        pokemon for pokemon in pokemons if "Fire" in pokemon["type"]
-    ]
 
-print(fire_type_pokemons[0])
+fire_type_pokemons = [
+    pokemon for pokemon in pokemons if "Fire" in pokemon["type"]
+]
 
 with open("fire_type_pokemons.txt", mode="w") as fire_pokemons:
-    json_write = json.dumps(fire_type_pokemons)
-    fire_pokemons.write(json_write)
+    json.dump(fire_type_pokemons, fire_pokemons)
