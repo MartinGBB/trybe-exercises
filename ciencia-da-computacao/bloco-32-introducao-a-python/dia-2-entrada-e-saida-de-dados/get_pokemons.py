037a145791a1272bf1873d7d70e744533b8b5cfd
Martin <escorpmartin97@gmail.com>
2022-02-10 18:07:41 -0300

    filtra pokemons de fuego

--- ciencia-da-computacao/bloco-32-introducao-a-python/dia-2-entrada-e-saida-de-dados/get_pokemons.py
+++ ciencia-da-computacao/bloco-32-introducao-a-python/dia-2-entrada-e-saida-de-dados/get_pokemons.py
@@ -2,5 +2,8 @@ import json
 
 with open("pokemons.json") as file:
     pokemons = json.load(file)["results"]
+    pokemons_type_fire = [
+        pokemon for pokemon in pokemons if "Fire" in pokemon["type"]
+    ]
 
-print(pokemons[0])
+print(pokemons_type_fire[0])
