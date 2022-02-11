b3a90ab3b0754e5eed5ba6e0cc37bf5a4a3b4a41
Martin <escorpmartin97@gmail.com>
2022-02-11 15:53:07 -0300

    testa excepcao de divisao

--- ciencia-da-computacao/bloco-32-introducao-a-python/dia-3-testes/test_codigo.py
+++ ciencia-da-computacao/bloco-32-introducao-a-python/dia-3-testes/test_codigo.py
@@ -14,3 +14,8 @@ def test_is_odd_when_number_is_even_returns_false():
 
 def test_divide_when_other_number():
     assert divide(4, 2) == 2
+
+
+def test_divide_when_other_number_is_zero_raises_an_exception():
+    with pytest.raises(ZeroDivisionError, match="division by zero"):
+        divide(2, 0)
