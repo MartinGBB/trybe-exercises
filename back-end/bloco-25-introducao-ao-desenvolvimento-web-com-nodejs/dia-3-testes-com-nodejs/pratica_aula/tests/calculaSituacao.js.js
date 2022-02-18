329d3b9fc1dd3579f5cb20bcc8a128b4eae81d6e
Martin <escorpmartin97@gmail.com>
2022-02-18 20:32:09 -0300

    fix: modifica describe para adicionar mais um teste dentro dela

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/pratica_aula/tests/calculaSituacao.js.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/pratica_aula/tests/calculaSituacao.js.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const calculaSituacao = require('../calculaSituacao');
 
-describe("testa resposta de calculaSituacao quando a media for menor que 7", () => {
+describe("testa resposta de calculaSituacao", () => {
   it("retorna 'reprovado'", () => {
     const resposta = calculaSituacao(3);
     expect(resposta).to.be.equals("reprovado");
