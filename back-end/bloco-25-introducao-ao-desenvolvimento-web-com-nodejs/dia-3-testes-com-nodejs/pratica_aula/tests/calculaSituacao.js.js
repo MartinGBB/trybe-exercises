7eb4e621cb506bb852ae4fd65d2461fe32f03399
Martin <escorpmartin97@gmail.com>
2022-02-18 20:30:14 -0300

    fix: corrige chai

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/pratica_aula/tests/calculaSituacao.js.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/pratica_aula/tests/calculaSituacao.js.js
@@ -2,8 +2,8 @@ const { expect } = require('chai');
 const calculaSituacao = require('../calculaSituacao');
 
 describe("testa resposta de calculaSituacao quando a media for menor que 7", () => {
-  it(("retorna 'reprovado'", () => {
+  it("retorna 'reprovado'", () => {
     const resposta = calculaSituacao(3);
-    expect(resposta).to.be("reprovado");
-  }));
+    expect(resposta).to.be.equals("reprovado");
+  });
 });
