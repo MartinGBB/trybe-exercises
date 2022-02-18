00a335d27bbb2d52b151140b94f7ecfd81579994
Martin <escorpmartin97@gmail.com>
2022-02-18 20:26:48 -0300

    feat: adiciona gitignore

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/pratica_aula/tests/calculaSituacao.js.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/pratica_aula/tests/calculaSituacao.js.js
@@ -3,6 +3,7 @@ const calculaSituacao = require('../calculaSituacao');
 
 describe("testa resposta de calculaSituacao quando a media for menor que 7", () => {
   it(("retorna 'reprovado'", () => {
-    expect(calculaSituacao(3)).to.be("reprovado");
+    const resposta = calculaSituacao(3);
+    expect(resposta).to.be("reprovado");
   }));
 });
