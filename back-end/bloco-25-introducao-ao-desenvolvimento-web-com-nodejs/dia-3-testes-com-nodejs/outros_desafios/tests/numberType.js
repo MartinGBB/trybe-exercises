b3395f74fed949b51adb38f4254e3d9602a0fa6a
Martin <escorpmartin97@gmail.com>
2022-02-19 15:34:55 -0300

    feat: adiciona mais um teste. fix: muda nome da funcao

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/outros_desafios/tests/numberType.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/outros_desafios/tests/numberType.js
@@ -1,18 +1,26 @@
 const { expect } = require('chai');
 
-const response = require('../numberType');
+const numberType = require('../numberType');
 
 describe("testa função numberType", () => {
+  describe("Recebe um numero", () => {
+    it("Deve retornar uma string", () => {
+
+    });
+  });
+
   describe("recebe numero positivo maior que zero", () => {
     it("Deve retornar 'positivo'", () => {
 
     });
   });
+
   describe("recebe numero negativo", () => {
     it("Deve retornar 'negativo'", () => {
 
     });
   });
+
   describe("recebe numero neutro", () => {
     it("Deve retornar 'neutro'", () => {
 
