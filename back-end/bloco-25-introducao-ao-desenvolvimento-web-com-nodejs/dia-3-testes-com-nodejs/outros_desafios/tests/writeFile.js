72583da06ac1932b2821776f743636a277886128
Martin <escorpmartin97@gmail.com>
2022-02-19 18:59:23 -0300

    feat: testa com sinon

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/outros_desafios/tests/writeFile.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/outros_desafios/tests/writeFile.js
@@ -7,6 +7,14 @@ const CONTEUDO_DO_ARQUIVO = "conteudo";
 
 describe("testa writeFile", () => {
   describe("A resposta", () => {
+    before(() => {
+    sinon.stub(fs, 'writeFileSync').returns(CONTEUDO_DO_ARQUIVO);
+    });
+
+    after(() => {
+      fs.writeFileSync.restore();
+    });
+
     it("È uma string", () => {
       const response = writeFile("arquivo.txt", CONTEUDO_DO_ARQUIVO);
       expect(response).to.be.a('string');
