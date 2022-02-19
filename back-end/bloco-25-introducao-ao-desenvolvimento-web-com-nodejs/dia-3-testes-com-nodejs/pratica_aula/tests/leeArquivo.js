bc8768f9e28e74f6fccfad304accadce78c80010
Martin <escorpmartin97@gmail.com>
2022-02-18 23:27:03 -0300

    feat: implementa mocha de teste

---
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/pratica_aula/tests/leeArquivo.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+const leArquivo = require('./leArquivo');
+
+describe('Testa função "leArquivo"', () => {
+  describe('Quando o arquivo existe', () => {
+    describe('A resposta', () => {
+      it('É uma string', () => {
+        
+      });
+
+      it('O arquivo é igual ao conteúdo do arquivo', () => {
+    
+      });
+    });
+  });
+
+  describe('Quando o arquivo não existe', () => {
+    describe('A resposta', () => {
+      it('É "null', () => {
+        
+      });
+    })
+  });
+
+});
