2a9225c47d8b3d4818347b6bae9c007cb232aedf
Martin <escorpmartin97@gmail.com>
2022-02-19 15:29:38 -0300

    feat: implementa mocha chai

---
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/outros_desafios/tests/numberType.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai');
+
+const response = require('../numberType');
+
+describe("testa função numberType", () => {
+  describe("recebe numero positivo maior que zero", () => {
+    it("Deve retornar 'positivo'", () => {
+
+    });
+  });
+  describe("recebe numero negativo", () => {
+    it("Deve retornar 'negativo'", () => {
+
+    });
+  });
+  describe("recebe numero neutro", () => {
+    it("Deve retornar 'neutro'", () => {
+
+    });
+  });
+});
