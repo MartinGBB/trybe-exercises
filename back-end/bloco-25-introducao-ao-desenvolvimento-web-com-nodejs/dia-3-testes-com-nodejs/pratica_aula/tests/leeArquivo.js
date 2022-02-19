241f47c1d518e5d01c989ce5a48bfadc5602b514
Martin <escorpmartin97@gmail.com>
2022-02-18 23:32:27 -0300

    feat: implementa chai, testa que o conteudo e string

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/pratica_aula/tests/leeArquivo.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/pratica_aula/tests/leeArquivo.js
@@ -1,11 +1,14 @@
 const { expect } = require('chai');
 const leArquivo = require('./leArquivo');
 
+const CONTEUDO_DO_ARQUIVO = 'arquivo para ler';
+
 describe('Testa função "leArquivo"', () => {
   describe('Quando o arquivo existe', () => {
     describe('A resposta', () => {
       it('É uma string', () => {
-        
+        const resposta = leArquivo(CONTEUDO_DO_ARQUIVO);
+        expect(resposta).to.be.a('string');
       });
 
       it('O arquivo é igual ao conteúdo do arquivo', () => {
