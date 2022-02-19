218e75711a46507ce66d49d04cc83b72ec0d75d8
Martin <escorpmartin97@gmail.com>
2022-02-19 00:33:48 -0300

    fix: corrige erro de implementacao para ler arquivo

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/pratica_aula/leArquivo.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/pratica_aula/leArquivo.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const leArquivo = (arquivo) => {
   try {
-    const conteudoDoArquivo = fs(arquivo, 'utf8');
+    const conteudoDoArquivo = fs.readFileSync(arquivo, 'utf8');
     return conteudoDoArquivo;
   } catch(err) {
     return null;
