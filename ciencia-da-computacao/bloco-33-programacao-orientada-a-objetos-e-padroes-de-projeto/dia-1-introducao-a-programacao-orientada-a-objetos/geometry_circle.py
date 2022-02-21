9a489c00f58afae8134f7a87af328546b041727f
Martin <escorpmartin97@gmail.com>
2022-02-21 20:32:09 -0300

    feat: adiciona perimetro

--- ciencia-da-computacao/bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-1-introducao-a-programacao-orientada-a-objetos/geometry_circle.py
+++ ciencia-da-computacao/bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-1-introducao-a-programacao-orientada-a-objetos/geometry_circle.py
@@ -8,7 +8,11 @@ class Circle:
     def area(self):
         return self.radius * PI
 
+    def perimeter(self):
+        return 2 * PI * self.radius
+
 
 calculate_circle = Circle(5)
 
 print(calculate_circle.area())
+print(calculate_circle.perimeter())
