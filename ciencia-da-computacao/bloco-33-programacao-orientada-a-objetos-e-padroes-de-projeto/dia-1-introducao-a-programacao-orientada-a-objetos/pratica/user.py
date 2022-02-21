d55e5cc4f7095c8df071b4dac3b99bab6dfc0ad2
Martin <escorpmartin97@gmail.com>
2022-02-21 15:09:50 -0300

    feat: minha primeira entidade

---
+++ ciencia-da-computacao/bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-1-introducao-a-programacao-orientada-a-objetos/pratica/user.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+
+meu_user = User("Valentino Trocatapa", "valentino@tinytoons.com", "Grana")
