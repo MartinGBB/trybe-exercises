c8286099a0ca31f9083a37d5a91c2c4ba882f207
Martin <escorpmartin97@gmail.com>
2022-02-21 23:19:06 -0300

    feat: retorna total do pedido

--- ciencia-da-computacao/bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-1-introducao-a-programacao-orientada-a-objetos/cafeteria.py
+++ ciencia-da-computacao/bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-1-introducao-a-programacao-orientada-a-objetos/cafeteria.py
@@ -1,14 +1,29 @@
 class Item:
-    def __init__(self, produto, preco, desconto):
-        self.produto = produto
+    def __init__(self, product, preco):
+        self.product = product
         self.preco = preco
-        self.desconto = desconto
 
 
 class Pedido:
-    def __init__(self, cliente, items, preco, desconto):
+    def __init__(self, cliente, items, desconto):
         self.cliente = cliente
-        self.pedido = items
-        self.preco = preco
+        self.items = items
         self.desconto = desconto
-        
+
+    def calcula_total(self):
+        total = 0
+        for item in self.items:
+            total += item.preco
+        return total
+
+    def total_desconto(self):
+        return self.calcula_total() * (1 - self.desconto)
+
+
+sanduba = Item("X-tudo", 16.9)
+guarana = Item("Guarana", 5.9)
+fritas = Item("Fritas crocantes", 10.9)
+
+pedido_mesa_1 = Pedido("Cristiano", [sanduba, guarana, fritas], 0.1)
+
+print(f'Total: R$ {pedido_mesa_1.total_desconto()}')
