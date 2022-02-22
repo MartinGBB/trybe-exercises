738bd66b6921642582cc5915a5aa6829ca50bd6a
Martin <escorpmartin97@gmail.com>
2022-02-21 22:48:43 -0300

    feat: cria sistema de vendas de uma cafeteria

---
+++ ciencia-da-computacao/bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-1-introducao-a-programacao-orientada-a-objetos/cafeteria.py
@@ -0,0 +1,10 @@
+from itertools import product
+
+
+class Item:
+    def __init__(self, produto, preco, desconto):
+        self.produto = produto
+        self.preco = preco
+        self.desconto = desconto
+
+
