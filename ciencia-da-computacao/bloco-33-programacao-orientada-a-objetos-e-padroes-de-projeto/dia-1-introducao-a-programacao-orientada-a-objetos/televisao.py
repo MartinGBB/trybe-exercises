25df712725143c1e748ccf347eed5c70f5a3d5ec
Martin <escorpmartin97@gmail.com>
2022-02-23 16:29:00 -0300

    feat: aumenta e diminui volumen

--- ciencia-da-computacao/bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-1-introducao-a-programacao-orientada-a-objetos/televisao.py
+++ ciencia-da-computacao/bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-1-introducao-a-programacao-orientada-a-objetos/televisao.py
@@ -1,10 +1,20 @@
 class Televisao:
-    def __init__(self, on_off, ch_up, ch_down, vol_up, vol_down):
+    def __init__(
+        self,
+        on_off,
+        ch_up,
+        ch_down,
+        ch_atual,
+        vol_atual
+    ):
         self.on_off = on_off
         self.ch_up = ch_up
-        self.down = ch_down
+        self.ch_down = ch_down
+        self.ch_atual = ch_atual
         self.vol_up = vol_up
         self.vol.down = vol_down
+        self.vol_atual = vol_atual
+        self.taxa_vol = 10
 
     def liga_tv(self):
         self.on_off = True
@@ -13,4 +23,10 @@ class Televisao:
     def desliga_tv(self):
         self.on_off = False
         print("Tv desligada")
+
+    def aumentar_volumem(self):
+        self.vol_atual += self.taxa_vol
+
+    def diminuir_volumem(self):
+        self.vol_atual -= self.taxa_vol
 
