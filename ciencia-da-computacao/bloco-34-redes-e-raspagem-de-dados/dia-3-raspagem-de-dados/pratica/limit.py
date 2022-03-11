0927e3bafc93030c91413b2031250a43fe9aca2e
Martin <escorpmartin97@gmail.com>
2022-03-11 19:59:09 -0300

    feat: retorna com timeout e try/except

--- ciencia-da-computacao/bloco-34-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/pratica/limit.py
+++ ciencia-da-computacao/bloco-34-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/pratica/limit.py
@@ -6,3 +6,10 @@ for _ in range(15):
     response = requests.get("https://www.cloudflare.com/rate-limit-test/")
     print(response.status_code)
     time.sleep(6)
+
+try:
+    response = requests.get("http://httpbin.org/delay/10", timeout=2)
+except requests.ReadTimeout:
+    response = requests.get("http://httpbin.org/delay/1", timeout=2)
+finally:
+    print(response.status_code)
