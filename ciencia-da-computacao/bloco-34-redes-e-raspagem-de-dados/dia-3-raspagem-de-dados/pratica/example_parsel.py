151fc1b095ffc54e0ec14bdd2338b542a6cb7dfb
Martin <escorpmartin97@gmail.com>
2022-03-13 14:16:09 -0300

    feat: pega dados das outras pages

--- ciencia-da-computacao/bloco-34-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/pratica/example_parsel.py
+++ ciencia-da-computacao/bloco-34-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/pratica/example_parsel.py
@@ -1,18 +1,24 @@
 from parsel import Selector
 import requests
 
-response = requests.get("http://books.toscrape.com/")
-selector = Selector(text=response.text)
-titles = selector.css(".product_pod h3 a::attr(title)").getall()
-prices = selector.css(".product_price .price_color::text").getall()
-# print(titles)
-# print(prices)
+# response = requests.get("http://books.toscrape.com/")
+# selector = Selector(text=response.text)
+# titles = selector.css(".product_pod h3 a::attr(title)").getall()
+# prices = selector.css(".product_price .price_color::text").getall()
+# # print(titles)
+# # print(prices)
 
+URL_BASE = "http://books.toscrape.com/catalogue/"
+url_next_page = 'page-1.html'
 
-# for product in selector.css(".product_pod"):
-#     titles = product.css("h3 a::attr(title)").get()
-#     prices = product.css(".product_price .price_color::text").get()
-#     print(titles, prices)
+while url_next_page:
+    response = requests.get(URL_BASE + url_next_page)
+    selector = Selector(text=response.text)
 
-next_page_url = selector.css(".next a::attr(href)").get()
-print(next_page_url)
+    for product in selector.css(".product_pod"):
+        titles = product.css("h3 a::attr(title)").get()
+        prices = product.css(".price_color::text").get()
+        print(titles, prices)
+
+    url_next_page = selector.css(".next a::attr(href)").get()
+    print(f"---------------- NEXT PAGE ({url_next_page}) ------------------")
