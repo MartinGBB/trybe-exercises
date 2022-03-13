59582fc966f99c9f5fa14ef3bf37c86b3f8a468e
Martin <escorpmartin97@gmail.com>
2022-03-13 13:26:47 -0300

    fix: organiza codigo com variavels feat: adiciona codigo que pega dados de preço

--- ciencia-da-computacao/bloco-34-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/pratica/example_parsel.py
+++ ciencia-da-computacao/bloco-34-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/pratica/example_parsel.py
@@ -3,4 +3,7 @@ import requests
 
 response = requests.get("http://books.toscrape.com/")
 selector = Selector(text=response.text)
-print(selector.css(".product_pod h3 a::attr(title)").getall())
+titles = selector.css(".product_pod h3 a::attr(title)").getall()
+prices = selector.css(".product_price .price_color::text").getall()
+print(titles)
+print(prices)
