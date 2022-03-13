5d5d77416a97727d4366fd2ee9236d9f1e593467
Martin <escorpmartin97@gmail.com>
2022-03-13 14:16:32 -0300

    fix: remove codigo comentado

--- ciencia-da-computacao/bloco-34-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/pratica/example_parsel.py
+++ ciencia-da-computacao/bloco-34-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/pratica/example_parsel.py
@@ -1,13 +1,6 @@
 from parsel import Selector
 import requests
 
-# response = requests.get("http://books.toscrape.com/")
-# selector = Selector(text=response.text)
-# titles = selector.css(".product_pod h3 a::attr(title)").getall()
-# prices = selector.css(".product_price .price_color::text").getall()
-# # print(titles)
-# # print(prices)
-
 URL_BASE = "http://books.toscrape.com/catalogue/"
 url_next_page = 'page-1.html'
 
